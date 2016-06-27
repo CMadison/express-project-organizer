d347ff5f1795b98e1df67069f011faeef808f8b8
Chris Madison <CKMadison4@gmail.com>
2016-06-27 10:04:13 -0700

    Added associations for the story and user models. Next steps are to setup post route for a story.

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,12 +2,15 @@ var express = require('express');
 var db = require('../models');
 var router = express.Router();
 
+// Find all categories and display them
 router.get('/', function(req, res) {
   db.category.findAll().then(function(category){
   res.render('categories/show.ejs', { category: category });
   })
 });
 
+
+// Find all projects associated with a particular category, and display them.
 router.get('/:name', function(req, res) {
   db.category.find({
     where: { name: req.params.name },
